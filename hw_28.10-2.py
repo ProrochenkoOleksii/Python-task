c410954abf48e7e47e1885e0100561ca5de18372
Бігоцька Юля <bigockaya@MacBook-Bigocka.local>
2023-10-29 18:17:08 +0200

    2 part

---
+++ hw_28.10-2.py
@@ -0,0 +1,30 @@
+# exercise1
+string = "hello world"
+lenght = len(string)
+string_new = string[0:2] + string[lenght-2:lenght]
+print (string_new)
+
+string_1 = "my"
+lenght_1 = len(string_1)
+string_neww = string_1[0:2] + string_1[lenght_1-2:lenght_1]
+print (string_neww)
+
+string_2 = "x"
+if len(string_2)<2:
+    print("Empty String")
+else:
+    pass
+
+# exercise2
+phone = 4638495736
+if len(phone)<10:
+    print (f"Phone number {phone} is TRUE")
+else:
+    print (f"Phone number {phone} is FALSE")
+
+#exercise3
+
+
+#exercise4
+
+
