923cad9154da8b15ae027b44462f6dfac74e7213
Бігоцька Юля <bigockaya@MacBook-Bigocka.local>
2023-10-30 19:48:06 +0200

    class

--- cw_30.10.py
+++ cw_30.10.py
@@ -1,5 +1,5 @@
  
-string=input ("Input transaction string ")
+string=str(input ("Input transaction string "))
 if string[0]==";" and string[-1]=="%":
     string = string[1:len(string)-1]
 elif string[0]==";":
@@ -7,3 +7,13 @@ elif string[0]==";":
 elif string[-1]=="%":
     string = string[0:len(string)-1]
 print (string)
+
+money=int(input("how much money per month? "))
+age=int(input("your age "))
+job=input("do you have a job?")
+if money>100 and age>18 and job=="yes":
+    print ("Congratulations! Money are your.")
+elif money>1000 and age>20 and job=="no":
+    print ("Congratulations! Money are your.")
+else:
+    print ("Without money")
