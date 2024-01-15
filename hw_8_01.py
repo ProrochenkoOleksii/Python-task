4ccb0373e6ae51d59b272a00b6e2baa8a9126e56
Бігоцька Юля <bigockaya@MacBook-Bigocka.local>
2024-01-15 19:47:12 +0200

    hw

--- hw_8_01.py
+++ hw_8_01.py
@@ -1,11 +1,11 @@
 #1. robots.txt
 # Завантажте та збережіть файл robots.txt із веб-сайтів Вікіпедії, Twitter тощо.
 
-import requests
+# import requests
 
 # r = requests.get('https://www.wikipedia.org/')
 # r_s=str(r.status_code)
-# r_h=r.headers['content-type']
+# r_h=r.headers
 # r_t=r.text
 
 
@@ -26,18 +26,18 @@ import json
 
 r1 = requests.get("https://russianwarship.rip/api/v2/terms/en")
 r_s1 = str(r1.status_code)
-r_h1 = r1.headers['content-type']
-r_t1 = r1.text
-
+r_h1 = r1.headers
+# r_t1 = r1.text
+r_j1 = r1.json()
 with open ("file_url1.json", "w") as my_file:
-    json.dump({"status": r_s1, "head": r_h1, "text": r_t1}, my_file)
+    json.dump(r_j1,my_file)
     
-
 
 r2 = requests.get("https://russianwarship.rip/api/v2/war-info/status")
 r_s2 = str(r2.status_code)
-r_h2 = r2.headers['content-type']
-r_t2 = r2.text
+r_h2 = r2.headers
+# r_t2 = r2.text
+r_j2 = r2.json()
+with open ("file_url2.json", "w") as my_file2:
+    json.dump(r_j2,my_file2)
 
-with open ("file_url2.json", "w") as my_file:
-    json.dump({"status": r_s2, "head": r_h2, "text": r_t2}, my_file)
