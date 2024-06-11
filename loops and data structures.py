e361a83ff6449d13015f91327948b15367fefaa3
Prorochenko Oleksii <148998488+ProrochenkoOleksii@users.noreply.github.com>
2024-06-11 11:19:01 +0300

    Update loops and data structures.py

--- loops and data structures.py
+++ loops and data structures.py
@@ -153,6 +153,30 @@ else:
     print ("FALSE")
 
 
+#exercise5
+
+import random
+random.randint(1,10)
+data = int (input("Input number from 1 to 10:"))
+print (random.randint(1,10))
+if data==random.randint(1,10):
+    print ("You are the winner")
+else:
+    print ("You are the looser")
+
+
+#exercise6
+name = input ("Input your name ")
+age = int (input ("Input your age "))
+age_next = age + 1
+print (f"Hello, {name}, on your next birthday you will be {age_next} years")
+
+#exercise7
+import random
+string = input ("Input your string:")
+result_str = ''.join((random.choice(string)) for i in range(len(string)))
+print(result_str)
+
 
 # Tasks on data structures in Python:
 
