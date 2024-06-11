caafec8a955f0f8198cf05e6d08e7ae4a663c2b0
Prorochenko Oleksii <148998488+ProrochenkoOleksii@users.noreply.github.com>
2024-06-11 11:14:41 +0300

    Update loops and data structures.py

--- loops and data structures.py
+++ loops and data structures.py
@@ -109,6 +109,51 @@ while n<len(list5):
     n+=1
 print("\n")
 
+# exercise1
+string = "hello world"
+lenght = len(string)
+string_new = string[0:2] + string[lenght-2:lenght]
+print (string_new)
+
+string_1 = "my"
+lenght_1 = len(string_1)
+string_neww = string_1[0:2] + string_1[lenght_1-2:lenght_1]
+print (string_neww)
+
+string_2 = "x"
+if len(string_2)<2:
+    print("Empty String")
+else:
+    pass
+
+# exercise2
+num = "4638495736"
+l = len(num)
+if l <= 10:
+    print (f"Phone number {num} is TRUE")
+else:
+    print (f"Phone number {num} is FALSE")
+
+#exercise3
+x=21+35-17
+ans = input ("Answer is:")
+if ans != 39:
+    print ("You are loose")
+else:
+    print ("You are wright")
+
+
+#exercise4
+name = "oleksii"
+name_new = input ("input your name:")
+name_new_d=str.casefold (name_new)
+if name == name_new_d:
+    print ("TRUE")
+else:
+    print ("FALSE")
+
+
+
 # Tasks on data structures in Python:
 
 # 1. Create a list containing your favorite colors.
